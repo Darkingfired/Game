0dfba58cc3131856744fe8b6ae1b03ada2f873d1
D-cyan <102008300+Darkingfired@users.noreply.github.com>
2022-12-10 21:46:13 +0700

    Delete .history directory

--- .history/Game_20221207170915.js
+++
@@ -1,8 +0,0 @@
-function draw() {
-  var ctx = document.getElementById('myCanvas').getContext('2d')
-  var img = new Image()
-  img.src = '2c.jpg'
-  img.src = './hangman_copy.jpg'
-
-  ctx.drawImage(img, 0, 0)
-}
