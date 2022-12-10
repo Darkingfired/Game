f78efad4abc0c220ea3df14a13766ee6f6607688
Darkingfired <ngothienphucc@gmail.com>
2022-12-10 21:41:23 +0700

    up code

---
+++ .history/Game_20221112203335.js
@@ -0,0 +1,17 @@
+// const canvas = document.getElementById("myCanvas");
+// const ctx = canvas.getContext("2d");
+
+
+// var image = new Image();
+//image.src = './hangman copy.jpg';
+
+
+// window.onload = function()
+// {
+//   image.src = './hangman copy.jpg';
+//   const canvas = document.getElementById("myCanvas");
+//   const ctx = canvas.getContext("2d");
+//   const image = document. getElementById("myImage");
+//   ctx.drawImage(image, 1, 1);
+// }
+const myCanvas = document.getElementById('my-canvas'); const myContext = myCanvas.getContext('2d');
---
+++ .history/Game_20221112203605.js
@@ -0,0 +1,19 @@
+// const canvas = document.getElementById("myCanvas");
+// const ctx = canvas.getContext("2d");
+
+
+// var image = new Image();
+//image.src = './hangman copy.jpg';
+
+
+ window.onload = function()
+{
+//   image.src = './hangman copy.jpg';
+//   const canvas = document.getElementById("myCanvas");
+//   const ctx = canvas.getContext("2d");
+//   const image = document. getElementById("myImage");
+//   ctx.drawImage(image, 1, 1);
+context.drawImage('./cat.jpg', 0, 0);
+}
+const canvas = document.getElementById('canvas'); const context = canvas.getContext('2d'); const img = new Image();        img.src = './cat.jpg';        img.onload = () => {          context.drawImage(img, 0, 0);        };
+const myCanvas = document.getElementById('my-canvas'); const myContext = myCanvas.getContext('2d');
